1d4f81003443910279aa33ca3c14bcce5cc48941
Raj Poudel <raj.poudeel@gmail.com>
2023-06-09 22:18:07 +0545

    question inside var with object and arrrays inside adde

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 
-import './App.css';
+import './index.css'
 
 function App() {
-  return (
-    <div className="App">
+  const questions = [
+    {
+      questionText:'What is the capital of France?',
+      answerOptions:[
+        
+         { answerText:'New York',isCorrect:false},
+          {answerText:'London',isCorrect:false},
+          {answerText:'Paris',isCorrect:true},
+          {answerText:'Dublin',isCorrect:false},
+      
+      ],
+    },
+    {
+      questionText:'Who is CEO of Tesla?',
+      answerOptions: [
+        
+         { answerText:'Jeff Bezos',isCorrect:false },
+          { answerText:'Bill Gates',isCorrect:false },
+          { answerText:'Elon Musk',isCorrect:true },
+          { answerText:'Tony Stark',isCorrect:false },
+        
+      ],
+    },
+    {
+      questionText:'The iphone was created by which company?',
+      answerOptions:[
+        
+         { answerText:'Apple',isCorrect:true},
+          {answerText:'Intel',isCorrect:false},
+          {answerText:'Amazon',isCorrect:false},
+          {answerText:'Microsoft',isCorrect:false},
+        
+      ],
+    },
+  ];
 
+
+  // console.log(questions)
+  return (
+    <div className="app">
+      
     </div>
   );
 }
