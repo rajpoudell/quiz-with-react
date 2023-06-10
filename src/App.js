3ceb55d5172f646af6d413895f67a32527c4c08c
Raj Poudel <raj.poudeel@gmail.com>
2023-06-10 09:58:52 +0545

    commenting the useState what is being used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import './index.css'
 
 function App() {
@@ -49,29 +49,44 @@ function App() {
       ],
     },
   ];
-const [currentQuestion,setCurrentQuestion]=useState(0);
+const [currentQuestion,setCurrentQuestion]=useState(0); {/* this hook is used for nexting question with handleclick */}
 
-const handleAnswerButtonClick = () =>{
-  const nextQuestion = currentQuestion + 1;
-  setCurrentQuestion(nextQuestion)
+const [showScore,setShowScore]=useState(false); {/*this is used for the score that is show 
+                                                 at when the condition is end of the question  with alert msg*/}
+
+const [score,setScore]=useState(0); {/* this is being used for setting score when right question is being clicked */}
+
+const handleAnswerButtonClick = (isCorrect) =>{
+  if(isCorrect===true){ 
+    // alert("this answer is correct")
+    setScore(score+1) 
+  }
+  const nextQuestion = currentQuestion + 1; 
+  if(nextQuestion<questions.length){ {/*this logic is used for next question and when reached end of the question
+                                       else condition is come into action */}
+    setCurrentQuestion(nextQuestion)
+  }else{
+    alert("You have reached the end of the quiz")
+    setShowScore(true) //this is being used for showing the class="score section" at end
+  }
 }
 
   return (
     <div className="app">
-      {false ?(
-        <div className="score-section">You scored {} out of {questions.length}</div>
+      {showScore ?(
+        <div className="score-section">You scored {score} out of {questions.length}</div>
       ) : (
 
       <div className='all'>
         <div className="question-section">
           <div className="question-count">
-            <span>Question 1</span> /{questions.length}
+            <span>Question {}</span> /{questions.length}
           </div>
-          <div className="question-text">{questions[currentQuestion].questionText}</div>
+          <div className="question-text">{questions[currentQuestion].questionText}</div>  {/* currentQuestion as array index   is being used*/}
         </div>
         <div className="answer-section">
           {questions[currentQuestion].answerOptions.map((answerOptions) => 
-             <button onClick={handleAnswerButtonClick}>{answerOptions.answerText}</button>
+             <button onClick={()=> handleAnswerButtonClick(answerOptions.isCorrect)}>{answerOptions.answerText}</button>
           )}
            
           
